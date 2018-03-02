6ac202afdb487a14254edc26d5e45cfa28ba6516
Dmytro Ivanets <Dmytro_Ivanets@epam.com>
2018-03-02 13:05:38 +0200

    Initial commit

---
+++ src/test/java/qa/tasks/Dialog.java
@@ -0,0 +1,28 @@
+package qa.tasks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static qa.tasks.Maths.*;
+public class Dialog {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please Enter value for a");
+        int a = Integer.parseInt(reader.readLine());
+
+        System.out.println("Please Enter value for b");
+        int b = Integer.parseInt(reader.readLine());
+
+        System.out.println("Please Enter value for c");
+        int c = Integer.parseInt(reader.readLine());
+
+        System.out.println("a=" + a + ", b=" + b + ", c=" + c);
+
+        // Call Calculate from class Maths
+        Calculate(a, b, c);
+
+
+    }
+}
---
+++ src/test/java/qa/tasks/Maths.java
@@ -0,0 +1,21 @@
+package qa.tasks;
+
+
+import static qa.tasks.TestsForMath.Tests;
+
+public class Maths {
+
+    public static void Calculate(double a, double b, double c) {
+        double x1, x2;
+
+        Tests(a, b, c);
+
+        x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / 2 * a;
+        x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / 2 * a;
+
+            System.out.println("X1= " + x1);
+            System.out.println("X2= " + x2);
+        }
+
+
+}
