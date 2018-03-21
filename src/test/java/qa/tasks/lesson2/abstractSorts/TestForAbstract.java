756e3c651582da2947fbc474fb0f896b0ab0385b
Dmytro Ivanets <Dmytro_Ivanets@epam.com>
2018-03-21 16:30:46 +0200

    update 06.03.2018

--- src/test/java/qa/tasks/lesson2/abstractSorts/TestForAbstract.java
+++ src/test/java/qa/tasks/lesson2/abstractSorts/TestForAbstract.java
@@ -9,35 +9,32 @@ import qa.tasks.lesson2.abstractSort.QuickSort;
 
 public class TestForAbstract {
 
-
-
-
+    
     @Test
-    public void checkBSort(){
+    public void checkBSort() {
         int[] array = {2, 1, -2, 4, 3, 6, 0, -1, 1, 5};
         AbstractSorts sort = new BubbleSort(array);
         sort.sort();
         sort.getArray();
         int[] newArr = sort.getArray();
         Assert.assertEquals(newArr[0], -2);
-        Assert.assertEquals(newArr[newArr.length-1], 6);
+        Assert.assertEquals(newArr[newArr.length - 1], 6);
         Assert.assertSame(newArr[3], newArr[4]);
 
     }
 
     @Test
-    public void checkQSort(){
+    public void checkQSort() {
         int[] array = {2, 1, -2, 4, 3, 6, 0, -1, 1, 5};
         AbstractSorts sort = new QuickSort(array);
         sort.sort();
         sort.getArray();
         int[] newArr = sort.getArray();
         Assert.assertEquals(newArr[0], -2);
-        Assert.assertEquals(newArr[newArr.length-1], 6);
+        Assert.assertEquals(newArr[newArr.length - 1], 6);
         Assert.assertSame(newArr[3], newArr[4]);
 
     }
-
 
 
 }
