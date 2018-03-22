8277adf0eba36937d273d2aeb4d2399413a9e534
Dmytro Ivanets <Dmytro_Ivanets@epam.com>
2018-03-22 13:09:33 +0200

    update 22.03.2018

---
+++ src/main/java/qa/tasks/lesson2/additionalTasks/SumOfTheSeries.java
@@ -0,0 +1,13 @@
+package qa.tasks.lesson2.additionalTasks;
+
+public class SumOfTheSeries {
+    public static void main(String[] args) {
+        double sum = 1;
+        int n = 5000;
+
+        for (int i = 2; i <= n; i++) {
+            sum += (double)1/i;
+        }
+        System.out.println("Sum of the series = " + sum);
+    }
+}
